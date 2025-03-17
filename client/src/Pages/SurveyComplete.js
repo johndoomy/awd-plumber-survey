38370f6df4317fa1b073e95c85bb0974c6ac76c1
John <98440761+johndoomy@users.noreply.github.com>
2025-03-16 19:30:43 -0600

    Update survey submitiion response

--- client/src/Pages/SurveyComplete.js
+++ client/src/Pages/SurveyComplete.js
@@ -1,5 +1,16 @@
+import Grid from '@mui/material/Grid2';
+
 function SurveyComplete() {
-  return <p>Survey Complete</p>;
+  return (
+    <div>
+      <Grid sx={{ py: '20px' }} display="flex" justifyContent="center">
+        <p>Survey Complete</p>
+      </Grid>
+      <Grid sx={{}} display="flex" justifyContent="center">
+        <p>Thank you for your input!</p>
+      </Grid>
+    </div>
+  );
 }
 
 export default SurveyComplete;
